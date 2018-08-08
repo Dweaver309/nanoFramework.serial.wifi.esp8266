ba694fe5d08614e642d67f5ad91f9fd7d30cd10c
David E Weaver <dweaver309@gmail.com>
2018-08-08 18:09:41 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿
+//************
+//
+// This code was tested using 
+//
+//************
+//
+// ESP8266 Serial WIFI Witty ESP-12F
+//
+// When connected to my computer 5 volts were required
+//
+// Firmware version
+// AT version:1.3.0.0(Jul 14 2016 18:54:01)
+// SDK version:2.0.0(5a875ba)
+// v1.0.0.3
+// Mar 13 2018 09:37:06
+//
+//***********
+//
+// ESP-WROOM-S2 Wireless Wi-Fi Module
+//
+// When connected to my computer 3.3 volts were adequate
+//
+// Firmware version
+// AT version:1.1.0.0(May 11 2016 18:09:56)
+// SDK version:1.5.4(baaeaebb)
+// compile time:May 20 2016 15:31:17
+//
+//*************
+
+
+
+using System;
+using System.Threading;
+using nanoframework.serial.driver;
+using Windows.Devices.Gpio;
+
+namespace nanoframework.serial
+{
+    public class Program
+    {
+        const string CrLf = "\r\n";
+
+        public static void Main()
+        {
+           
+           // Netduino 3 D7 pin
+           // PinNumber is a local method
+           GpioPin D7 = GpioController.GetDefault().OpenPin(PinNumber('A', 1));
+
+           // Netduino 3 D8 pin
+           GpioPin D8 = GpioController.GetDefault().OpenPin(PinNumber('A', 0));
+
+           // Local method not used in testing 
+           // Use if needed
+           // ResetDevice(D7, D8);
+          
+           // Constructor for ESP8266 serial WiFi
+           WiFi ESP8266 = new WiFi();
+
+            // Get firmware version
+            ESP8266.GetVersion();
+
+
+           ESP8266.Connect("weaver", "3098280065");
+
+            //***Rem Uncomment to set time
+            //ESP8266.SetTime();
+           
+            ESP8266.StartServer();
+
+            //***Rem Uncomment for AP mode
+           // ESP8266.StartAPMode();
+
+          Console.WriteLine("IP Address: >> " + ESP8266.GetIPAddress());
+
+            while (true)
+                {
+
+                Thread.Sleep(1000);
+
+                 }
+        }
+   
+        /// <summary>
+        /// GH_PD must be pulled high to enable the signal
+        /// GPIO0 must be pulled high, if set to low the chip will be in flash boot mode 
+        /// Toggle the pins to reset the chip
+        /// </summary>
+        private static void ResetDevice(GpioPin GIO0CpuPin, GpioPin CH_PDCpuPin)
+        {
+
+            GIO0CpuPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            CH_PDCpuPin.SetDriveMode(GpioPinDriveMode.Output);
+
+           GIO0CpuPin.Write(GpioPinValue.Low);
+
+            CH_PDCpuPin.Write(GpioPinValue.Low);
+
+            Thread.Sleep(2000);
+
+            GIO0CpuPin.Write(GpioPinValue.High);
+
+            CH_PDCpuPin.Write(GpioPinValue.High);
+
+            Thread.Sleep(2000);
+
+        }
+       
+        static int PinNumber(char port, byte pin)
+        {
+
+            if (port < 'A' || port > 'J')
+            {
+
+                throw new ArgumentException();
+
+            }
+
+            return ((port - 'A') * 16) + pin;
+
+        }
+
+    }
+
+}
+
+
---
+++ WebPages.cs
@@ -0,0 +1,98 @@
+﻿
+namespace nanoframework.serial
+{
+    class WebPages
+    {
+        private const string CrLf = "\r\n";
+
+        public static string DefaultPage(string Status)
+        {
+           
+            string t = string.Empty;
+
+            t += CrLf + "<html>";
+            t += CrLf + "<head>";
+            t += CrLf + "<meta http-equiv=\"Content-Language\" content=\"en-us\">";
+            t += CrLf + "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\">";
+            t += CrLf + "<meta name=\"theme-color\" content=\"#ffffff\">";
+            t += CrLf + "<span style=\"color:Gray;background-color:Transparent;border-color:White;font-family:Arial;font-size:Medium; left: 32px; position: absolute; top: 8px\">";
+            t += CrLf + "</span>";
+            t += CrLf + "<title>LED Controler</title>";
+            t += CrLf + "<meta name=\"viewport\" content=\"width=device-width; initial-scale=1.0; maximum-scale=1.0;\">";
+            t += CrLf + "</head>";
+            t += CrLf + "<body>";
+            t += CrLf + "<form method=\"GET\" action=\"default.html\">";
+            t += CrLf + "	<p align=\"left\"><font face=\"Arial\" size=\"4\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ";
+            t += CrLf + "	LED Controller</font></p>";
+            t += CrLf + "	<p><font face=\"Arial\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; LED 1:&nbsp;&nbsp; </font><input type=\"submit\" value=\"On\" name=\"LED1On\">&nbsp;&nbsp;&nbsp;";
+            t += CrLf + "	<font face=\"Arial\">";
+            t += CrLf + "	<input type=\"submit\" value=\"Off\" name=\"LED1Off\">&nbsp;&nbsp; </font></p>";
+            t += CrLf + "	<p><font face=\"Arial\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; LED 2:&nbsp;&nbsp; </font>";
+            t += CrLf + "	<input type=\"submit\" value=\"On\" name=\"LED2On\">&nbsp; &nbsp;";
+            t += CrLf + "	<font face=\"Arial\">";
+            t += CrLf + "	<input type=\"submit\" value=\"Off\" name=\"LED2Off\">&nbsp;&nbsp;</font></p>";
+            t += CrLf + "	<p><font face=\"Arial\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; LED 3:&nbsp;&nbsp; </font>";
+            t += CrLf + "	<input type=\"submit\" value=\"On\" name=\"LED3On\">&nbsp;&nbsp;&nbsp;";
+            t += CrLf + "	<font face=\"Arial\">";
+            t += CrLf + "	<input type=\"submit\" value=\"Off\" name=\"LED3Off\">&nbsp;&nbsp; </font></p>";
+            t += CrLf + "	<p><font face=\"Arial\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; LED 4:&nbsp;&nbsp; </font>";
+            t += CrLf + "	<input type=\"submit\" value=\"On\" name=\"LEDUserOn\">&nbsp;&nbsp;&nbsp;";
+            t += CrLf + "	<font face=\"Arial\">";
+            t += CrLf + "	<input type=\"submit\" value=\"Off\" name=\"LEDUserOff\">&nbsp;&nbsp; </font></p>";
+            t += CrLf + "	</form>";
+            t += CrLf + "	<p align=\"left\"><font face=\"Arial\" size=\"4\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ";
+            t += CrLf + Status + " </ font ></ p > ";
+            t += CrLf + "<p>&nbsp;</p>";
+            t += CrLf + "</body>";
+            t += CrLf + "</html>";
+
+
+            return t;
+        }
+
+        public static string RouterSettingsPage()
+        {
+            string t = string.Empty;
+
+            // t += "<html xmlns=""http://www.w3.org/1999/xhtml"" >"
+            t += CrLf + "<head><title>";
+            t += CrLf + "	Router Settings";
+            t += CrLf + "</title></head>";
+            t += CrLf + "<meta name=\"viewport\" content=\"width=device-width; initial-scale=1.0; maximum-scale=1.0;\"></head>";
+            t += CrLf + "<body bgcolor=\"whitesmoke\">";
+            t += CrLf + "    <form name=\"form1\" method=\"get\" action=\"RouterSettings.html\" id=\"form1\">";
+            t += CrLf + "    <div>";
+            t += CrLf + "        <span style=\"color:Gray;background-color:Transparent;border-color:White;font-family:Arial;font-size:Medium; left: 16px; position: absolute; top: 8px\">";
+            t += CrLf + "</span>";
+            // Title
+            t += CrLf + "        <span style=\"display:inline-block;font-family:Arial;font-size:Large;width:200px;";
+            t += CrLf + "            left: 30px; position: absolute; top: 16px\">Router Settings</span>";
+            // SSID
+            t += CrLf + "        <span  style=\"font-family:Arial;z-index: 100; left: 16px;";
+            t += CrLf + "            position: absolute; top: 56px\">SSID:</span>";
+            t += CrLf + "        <input name=\"SSID\" type=\"text\" value=\"\"  style=\"width:120px;z-index: 101; left: 120px; position: absolute;";
+            t += CrLf + "            top: 56px\" />";
+
+            // Password
+            t += CrLf + "        <span  style=\"display:inline-block;font-family:Arial;width:168px;z-index: 103; left: 16px;";
+            t += CrLf + "            position: absolute; top: 96px\">Password:</span>";
+            t += CrLf + "        <input name=\"Password\" type=\"text\" value=\"\"  style=\"width:120px;z-index: 104; left: 120px; position: absolute;";
+            t += CrLf + "            top: 96px\" />";
+
+            // Save SSID
+            t += CrLf + "        <input type=\"submit\" name=\"Save\" value=\"Save\"  style=\"width:104px;z-index: 102; left: 120px; position: absolute;";
+            t += CrLf + "            top: 146px\" />";
+            t += CrLf + "    ";
+            t += CrLf + "    </div>";
+            t += CrLf + "    </form>";
+            t += CrLf + "</body>";
+            t += CrLf + "</html>";
+            t += CrLf + CrLf;
+
+            return t;
+        }
+
+
+
+    }
+}
